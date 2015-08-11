eb4918e28c304f7de52f91990ab6b3f233c0458a
Russ Smith <rsmith@valoremconsulting.com>
2015-08-10 22:34:10 -0500

    Working on changing the GitHub pages

---
+++ Scripts/indexmain.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+
+
+    $("#btViewOnGitHub").click(function () {
+        window.location.href = "https://github.com/OfficeDev/Office-IT-Pro-Deployment-Scripts";
+        return false;
+    });
+
+    $("#btDownloadZip").click(function () {
+        window.location.href = "https://github.com/OfficeDev/Office-IT-Pro-Deployment-Scripts/zipball/master";
+        return false;
+    });
+
+});
